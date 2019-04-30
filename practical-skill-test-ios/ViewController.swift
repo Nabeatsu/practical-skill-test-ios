034b292fa663201f376e8ce09cb7466d584af617
tanabenobuyuki <tanabe.1478@gmail.com>
2019-05-01 05:45:07 +0900

    initial commit

---
+++ practical-skill-test-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  practical-skill-test-ios
+//
+//  Created by 田辺信之 on 2019/05/01.
+//  Copyright © 2019 田辺信之. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
